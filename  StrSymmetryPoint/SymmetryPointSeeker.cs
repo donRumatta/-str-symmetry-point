e58dbd907a1286b900d4dc262ca06e1b1198cd2c
Borya Frolov <bfrolov@renins.com>
2024-07-07 20:26:29 +0300

    the solution

---
+++  StrSymmetryPoint/SymmetryPointSeeker.cs
@@ -0,0 +1,35 @@
+﻿namespace StrSymmetryPoint;
+
+public static class SymmetryPointSeeker
+{
+    public static int Seek(string input)
+    {
+        if (input.Length == 1)
+        {
+            return 0;
+        }
+        
+        if (input.Length % 2 == 0)
+        {
+            return -1;
+        }
+        
+        var middle = (input.Length - 1) / 2;
+
+        var i = middle - 1;
+        var j = middle + 1;
+
+        while (j < input.Length)
+        {
+            if (input[i] != input[j])
+            {
+                return -1;
+            }
+
+            i -= 1;
+            j += 1;
+        }
+
+        return middle;
+    }
+}
---
+++  StrSymmetryPoint/SymmetryPointSeekerTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace StrSymmetryPoint;
+
+[TestFixture]
+public class SymmetryPointSeekerTests
+{
+    [Test]
+    public void Seek_Empty()
+    {
+        var result = SymmetryPointSeeker.Seek("");
+
+        result.Should().Be(-1);
+    }
+    
+    [Test]
+    public void Seek_Single()
+    {
+        var result = SymmetryPointSeeker.Seek("x");
+
+        result.Should().Be(0);
+    }
+    
+    [Test]
+    public void Seek_Even_Chars()
+    {
+        var result = SymmetryPointSeeker.Seek("xxxxxx");
+
+        result.Should().Be(-1);
+    }
+    
+    [Test]
+    public void Seek_Three_Chars_Pos()
+    {
+        var result = SymmetryPointSeeker.Seek("121");
+
+        result.Should().Be(1);
+    }
+    
+    [Test]
+    public void Seek_Three_Chars_Neg()
+    {
+        var result = SymmetryPointSeeker.Seek("x2y");
+
+        result.Should().Be(-1);
+    }
+    
+    [Test]
+    public void Seek_Long_Pos()
+    {
+        var result = SymmetryPointSeeker.Seek("racecar");
+
+        result.Should().Be(3);
+    }
+    
+    [Test]
+    public void Seek_Long_Neg()
+    {
+        var result = SymmetryPointSeeker.Seek("qwertyracecarytqwe");
+
+        result.Should().Be(-1);
+    }
+}
